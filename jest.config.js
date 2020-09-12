2b7d5159f48d25eb77018db78599e07a66baafe5
shenliwen <460529511@qq.com>
2020-09-12 11:15:30 +0800

    new

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,7 @@ module.exports = {
   // browser: false,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\WWW\\AppData\\Local\\Temp\\jest",
+  // cacheDirectory: "C:\\Users\\ADMINI~1\\AppData\\Local\\Temp\\jest",
 
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
